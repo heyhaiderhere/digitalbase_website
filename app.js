3d81f4228f1672af53a8d8f7ac23f13595a54cd9
Haider Ali <72439082+heyhaiderhere@users.noreply.github.com>
2023-02-01 05:46:50 +0500

    reafactor

--- app.js
+++ app.js
@@ -6,20 +6,6 @@ function navToggle() {
   nav.classList.toggle("responsive");
 }
 
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       document.querySelector(".navbar").classList.add("navbar-move");
-//     } else {
-//       document.querySelector(".navbar").classList.remove("navbar-move");
-//     }
-//   });
-// });
-
-// const trigger = document.querySelectorAll(".nav-trigger");
-
-// trigger.forEach((el) => observer.observe(el));
-
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 30) {
     document.querySelector(".navbar").classList.add("navbar-move");
