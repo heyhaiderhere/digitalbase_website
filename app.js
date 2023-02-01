601bac942f5f1088161beab83677e93189e3c266
Haider Ali <72439082+heyhaiderhere@users.noreply.github.com>
2023-02-01 22:08:16 +0500

    final commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", navToggle);
 function navToggle() {
   const nav = document.querySelector(".nav-list");
+  const ham = document.querySelector(".hamburger-icon");
+  ham.classList.toggle("open");
   nav.classList.toggle("responsive");
 }
 
